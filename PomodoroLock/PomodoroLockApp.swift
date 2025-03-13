380c50b03c53f70f6b7b204d15cd29d9c2494040
“YunQiAI” <excerpt.10.able@icloud.com>
2025-03-13 11:30:01 +0900

    Initial Commit

---
+++ PomodoroLock/PomodoroLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroLockApp.swift
+//  PomodoroLock
+//
+//  Created by Li Hongmin on 2025/03/13.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PomodoroLockTests/PomodoroLockTests.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroLockTests.swift
+//  PomodoroLockTests
+//
+//  Created by Li Hongmin on 2025/03/13.
+//
+
+import Testing
+@testable import PomodoroLock
+
+struct PomodoroLockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
